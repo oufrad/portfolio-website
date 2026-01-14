5e8c32b355faa4958b9f3b083e7b1abcc05960ff
oufrad <53473805+oufrad@users.noreply.github.com>
2026-01-14 16:56:00 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ReadingComponent } from './reading/reading.component';
+import { UsesComponent } from './uses/uses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'projects', component: ProjectsComponent},
+    {path: 'reading', component: ReadingComponent},
+    {path: 'uses', component: UsesComponent},
+    {path: 'articles', component: ReadingComponent},
+];
---
+++ src/app/uses/uses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uses',
+  standalone: true,
+  imports: [],
+  templateUrl: './uses.component.html',
+  styleUrl: './uses.component.css'
+})
+export class UsesComponent {
+
+}
